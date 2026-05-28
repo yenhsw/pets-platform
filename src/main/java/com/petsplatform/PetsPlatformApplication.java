5e336d96538f0ffa3855c6ea785927d1f8bea8b6
yenhs <yenhs@nhakhoasing.vn>
2026-05-28 23:52:24 +0700

    init spring boot base project

---
+++ src/main/java/com/petsplatform/PetsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.petsplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsPlatformApplication.class, args);
+	}
+
+}
